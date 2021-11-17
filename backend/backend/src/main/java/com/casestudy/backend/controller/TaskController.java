471e2562dbb888705e36c62c5cd0313b8addbf3a
ahakar <ahakar@gmail.com>
2021-11-16 21:27:59 -0500

    US 04, 05 & 06

--- backend/backend/src/main/java/com/casestudy/backend/controller/TaskController.java
+++ backend/backend/src/main/java/com/casestudy/backend/controller/TaskController.java
@@ -1,33 +1,45 @@
 package com.casestudy.backend.controller;
 
-import com.casestudy.backend.model.Tasks;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-//by team 1
+import com.casestudy.backend.model.Tasks;
+import com.casestudy.backend.repository.TaskRepository;
+
 @RestController
-@RequestMapping("/tasks")
 public class TaskController {
 
-    @Autowired
-    TaskRepository taskRepository;
+	@Autowired
+	TaskRepository taskRepository;
 
-    @GetMapping("/tasks/{id}")
-    public Tasks getTaskbyId(@PathVariable("id") int id) {
-        Optional<Tasks> tasks = taskRepository.findById(id);
-        if (tasks.isPresent()) {
-            return tasks.get();
-        } else {
-            return null;
-        }
-    }
+	@GetMapping("/tasks/{id}")
+	public Tasks getTaskbyId(@PathVariable("id") int id) {
+		Optional<Tasks> tasks = taskRepository.findById(id);
+		if (tasks.isPresent()) {
+			return tasks.get();
+		} else {
+			return null;
+		}
+	}
 
-    @GetMapping("/tasks")
-    public List<Tasks> getAllTasks() {
-        List<Tasks> tasks = new ArrayList<Tasks>();
-        taskRepository.findAll().forEach(tasks::add);
-        return tasks;
-    }
+	@GetMapping("/tasks")
+	public List<Tasks> getAllTasks(@RequestParam(required = false) Integer parent,
+			@RequestParam(required = false) Boolean isproject) {
+		List<Tasks> tasks = new ArrayList<Tasks>();
+		if (parent != null) {
+			taskRepository.findByParent(parent).forEach(tasks::add);
+		} else if (isproject != null) {
+			taskRepository.findByIsProject(isproject).forEach(tasks::add);
+		} else {
+			taskRepository.findAll().forEach(tasks::add);
+		}
+		return tasks;
+	}
 }
--- backend/backend/src/main/java/com/casestudy/backend/repository/TaskRepository.java
+++ backend/backend/src/main/java/com/casestudy/backend/repository/TaskRepository.java
@@ -1,10 +1,14 @@
 package com.casestudy.backend.repository;
 
-import com.casestudy.backend.model.Tasks;
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import com.casestudy.backend.model.Tasks;
+
 @Repository
 public interface TaskRepository extends CrudRepository<Tasks, Integer> {
-
+	List<Tasks> findByParent(int parent);
+	List<Tasks> findByIsProject(Boolean isProject);
 }
